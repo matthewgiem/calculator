b18b55756a1abb50b791def4aad1344c8bc5b6fe
Colin W Kingen and Matthew Giem <matthew@>
2016-06-01 11:49:36 -0700

    Added basic JS interfacing.

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,47 @@
-
 // function adds two numbers
 var add = function(number1, number2) {
-  number1 = parseInt(prompt("Enter a number to add: "));
-  number2 = parseInt(prompt("Enter another number: "));
 
-  result = number1 + number2;
-  alert(result);
+  return number1 + number2;
+
 };
 
 
 
 // function subtracts two numbers
 var subtract = function(number1, number2) {
-  number1 = parseInt(prompt("Enter a number to subtract from: "));
-  number2 = parseInt(prompt("Enter another number: "));
 
-  result = number1 - number2;
-  alert(result);
+  return number1 - number2;
+
 };
 
 
 
 // function multiples two numbers
-var multiple = function(number1, number2) {
-  number1 = parseInt(prompt("Enter a number to multiple: "));
-  number2 = parseInt(prompt("Enter another number: "));
+var multiply = function(number1, number2) {
 
-  result = number1 * number2;
-  alert(result);
+  return number1 * number2;
+
 };
 
 
 
 // function divides two numbers
 var divide = function(number1, number2) {
-  number1 = parseInt(prompt("Enter a number to divide: "));
-  number2 = parseInt(prompt("Enter another number: "));
 
-  result = number1 / number2;
-  alert(result);
+  return number1 / number2;
+
 };
+
+
+// back-end logic:
+
+$(document).ready(function()  {
+  $("form#add").submit(function(event)  {
+
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    alert(add(number1, number2));
+  });
+
+});
