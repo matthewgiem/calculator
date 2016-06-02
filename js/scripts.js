7b494e3e0fb4bebbdc279c2ce454424a1ec53619
Matthew Giem and Tony Fabery <tony@>
2016-06-02 14:52:28 -0700

    branched different operations using jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
+// declaring all variables for all inputs
+var calc;
+var number1;
+var number2;
+var result;
+
 // function adds two numbers
 var add = function(number1, number2) {
 
@@ -23,8 +29,6 @@ var multiply = function(number1, number2) {
 
 };
 
-
-
 // function divides two numbers
 var divide = function(number1, number2) {
 
@@ -32,16 +36,34 @@ var divide = function(number1, number2) {
 
 };
 
+// Decide which function to return
+var calculate = function() {
+  if(calc === "add") {
+    result = add(number1, number2);
+  } else if(calc === "subtract") {
+    result = subtract(number1, number2);
+  } else if(calc === "multiply") {
+    result = multiply(number1, number2);
+  } else if(calc === "divide") {
+    result = divide(number1, number2);
+  } else {
+    result = "What do you want to do with your numbers?????";
+  }
+};
 
 // back-end logic:
 
 $(document).ready(function()  {
-  $("form#add").submit(function(event)  {
-
+  $("form#calc").submit(function(event)  {
     event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-    alert(add(number1, number2));
+    calc = $("input:radio[name=fn]:checked").val();
+    number1 = parseInt($("#num1").val());
+    number2 = parseInt($("#num2").val());
+    console.log("num1 =" + number1); //debug
+    console.log("num2 =" + number2); //debug
+    console.log("fn =" + calc); //debug
+    calculate();
+    $("#output").text(result);
   });
 
 });
